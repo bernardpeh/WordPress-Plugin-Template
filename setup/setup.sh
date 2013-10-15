bbaf6be5ff14be1d2c00624bc9ec6773b9cc325c
Bernard Peh <bpeh@sitecritic.net>
2013-10-15 21:02:53 +1100

    fix syntax in setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,7 +16,7 @@ then
   This simple script sets up the naming convention of all files and classes within this directory to the name of the plugin you specify. 
   Run the command using using this convention:
     
-  Usage: ./setup/setup.sh MY_Cool_Project my_page_name "My Page Title"
+  Usage: ./setup/setup.sh MY_Cool_Project my_page_name My_Page_Title
     "
   exit 1;
 fi
@@ -24,8 +24,10 @@ fi
 
 FILE_NAME=`echo $1 | sed 's/_/-/g'`
 CLASS_FILE_NAME=Class-$FILE_NAME
-OBJ_NAME=$1;
+OBJ_NAME=$1
 PLUGIN_NAME=`echo $1 | sed 's/_/ /g'`
+PAGE_SLUG=$2
+PAGE_TITLE=`echo $3 | sed 's/_/ /g'`
 
 # rename the main file
 mv WordPress-Plugin-Template/WordPress-Plugin-Template.php WordPress-Plugin-Template/$FILE_NAME.php
@@ -38,7 +40,7 @@ mv WordPress-Plugin-Template/classes/post-types/Class-WordPress-Plugin-Template-
 find . -name '*.php' -type f | while read s; do sed -e "s/WordPress Plugin Template/$PLUGIN_NAME/g" -e "s/Class-WordPress-Plugin-Template/$CLASS_FILE_NAME/g" -e "s/WordPress_Plugin_Template/$OBJ_NAME/g" -i $s; done
 
 # replace page slug and title
-sed -e "s/PAGE_SLUG/$2/g" -e "s/PAGE_TITLE/$3/g" -i WordPress-Plugin-Template/classes/$CLASS_FILE_NAME-Settings.php
+sed -e "s/PAGE_SLUG/$PAGE_SLUG/g" -e "s/PAGE_TITLE/$PAGE_TITLE/g" -i WordPress-Plugin-Template/classes/$CLASS_FILE_NAME-Settings.php
 
 # finally, rename this project dir
 mv WordPress-Plugin-Template $FILE_NAME
