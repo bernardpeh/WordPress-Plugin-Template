b4bbf02fecad3e4ffddac928566932074998c3e7
Bernard Peh <bpeh@sitecritic.net>
2013-10-15 17:12:26 +1100

    reorg dir structure

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,17 +16,18 @@ CLASS_FILE_NAME=class-$FILE_NAME
 OBJ_NAME=$1;
 PLUGIN_NAME=`echo $1 | sed 's/_/ /g'`
 
-# rename this project dir
-mv ../../WordPress-Plugin-Template ../$FILE_NAME
-
 # rename the main file
-mv ../WordPress-Plugin-Template.php ../$FILE_NAME.php
+mv ../WordPress-Plugin-Template/WordPress-Plugin-Template.php ../WordPress-Plugin-Template/$FILE_NAME.php
 
 # rename classes file
-mv ../classes/Class-WordPress-Plugin-Template.php  ../classes/$CLASS_FILE_NAME.php
-mv ../classes/Class-WordPress-Plugin-Template-Settings.php ../classes/$CLASS_FILE_NAME_SETTINGS.php
-mv ../classes/post-types/Class-WordPress-Plugin-Template-Post-Type.php ../classes/post-types/$CLASS_FILE_NAME_POST_TYPE.php
+mv ../WordPress-Plugin-Template/classes/Class-WordPress-Plugin-Template.php  ../WordPress-Plugin-Template/classes/$CLASS_FILE_NAME.php
+mv ../WordPress-Plugin-Template/classes/Class-WordPress-Plugin-Template-Settings.php ../WordPress-Plugin-Template/classes/$CLASS_FILE_NAME-Settings.php
+mv ../WordPress-Plugin-Template/classes/post-types/Class-WordPress-Plugin-Template-Post-Type.php ../WordPress-Plugin-Template/classes/post-types/$CLASS_FILE_NAME-Post_Type.php
 
 find ../ -name '*.php' -type f | while read s; do sed -e "s/WordPress Plugin Template/$PLUGIN_NAME/g" -e "s/Class-WordPress-Plugin-Template/$CLASS_FILE_NAME/g" -e "s/WordPress_Plugin_Template/$OBJ_NAME/g" -i $s; done
+
+# rename this project dir
+mv ../WordPress-Plugin-Template ../$FILE_NAME
+
 
 echo 'All done. Now copy this dir to your wordpress dir and activate it."
